0cc8e92b1623c32a4edce0b2fc782ff53650c0e4
epsilon-0 <mathgeniusabhinav@gmail.com>
2017-10-31 17:34:05 -0400

    cleaned file for generating graph

--- src/BASHSCRIPT.sh
+++ src/BASHSCRIPT.sh
@@ -1,6 +1,8 @@
 # args : number of people, p, where to write
-python 1Bash_genGraph.py 10 .4 "Graph" # initializes graph, and generates the cliques
+# initializes graph, and generates the cliques
+python generate_Graph.py --n=10 --p=.4 --adjFile="../output/graph.adj" --cliqueFile="../output/cliques.txt"
 
 #args : number of people, mingroup size, max group size, where to read the all cliques, where to write the covers to(for abhinav to work on)
-python 2Bash_genRandCover.py 10 0 4 "Graphbronk.txt" "Cover.Txt" # generate a disjioint rand cover, and output the output to "COVER.txt"
+# generate a disjioint rand cover, and output the output to "COVER.txt"
+python 2Bash_genRandCover.py 10 0 4 "../output/cliques.txt" "Cover.Txt"
 #CURRENTLY, does not write to cover.txt
