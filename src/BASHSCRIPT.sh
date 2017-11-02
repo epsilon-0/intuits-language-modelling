21be023c43f4ad3dd467ab836b9fb6df5e6518d7
epsilon-0 <mathgeniusabhinav@gmail.com>
2017-11-02 17:58:38 -0400

    temporary working code for BASHSCRIPT

--- src/BASHSCRIPT.sh
+++ src/BASHSCRIPT.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
+#SBATCH --job-name=Intuits-Simulation
+#SBATCH --nodes=2
+#SBATCH --cpus-per-task=25
+#SBATCH --mem=50GB
+#SBATCH --time=72:00:00
+
 RANDOM=42
 
 n=10  # number of people
 p=0.2 # density of graph
 mn=2  # minimum size of clique
-mx=2  # maximum size of clique
+mx=6  # maximum size of clique
+
+workingDirectory="$1"
 
 # initializes graph, and generates the cliques
-python generate_graph.py --n=$n --p=$p --adjFile="../output/graph.adj" --cliqueFile="../output/cliques.txt"
+python generate_graph.py --n=$n --p=$p --adjFile="$workingDirectory/output/graph.adj" --cliqueFile="$workingDirectory/output/cliques.txt"
 
 # outputs random list of covers of the graph to do conversations
 function generate_covers {
-    python generate_random_cover.py --n=$n --mn=$mn --mx=$mx --rFile="../output/cliques.txt" --wFile="temp" --seed=$1
+    python generate_random_cover.py --n=$n --mn=$mn --mx=$mx --rFile="$workingDirectory/output/cliques.txt" --wFile="temp" --seed=$1
 }
 
-workingDirectory="."
 numberOfTimesteps=10
 
 for ((i=1;i<$numberOfTimesteps;i+=1))
@@ -24,4 +31,12 @@ do
     rand=$RANDOM
     time output=$(generate_covers $rand)
     readarray lines <<<"$output"
+    cp -r "$workingDirectory/$i" "$workingDirectory/$((i+1))"
+    for ((j=1;j<${#lines[@]};j+=1))
+    do
+        echo $j
+        #srun --ntasks=1 --cpus-per-task=1 --exclusive --mem=3Gb
+        python largeEchoChamber.py --seed=$RANDOM --readDirectory="$workingDirectory/$i" --writeDirectory="$workingDirectory/$((i+1))" --learnerNumbers="${lines[$j]}" &
+    done
+    wait
 done
--- src/largeEchoChamber.py
+++ src/largeEchoChamber.py
@@ -1,23 +1,59 @@
-import sys
+import argparse
 from util import readVecspaceFile, writeVecspaceFile, learnerProcess
 from vecspace_learner import VecspaceLearner
+import numpy as np
 
-# first command line argument is number of files
-# the first half of the remainder is input file names
-# the remaining is the output file names
+# command line arguments
+parser = argparse.ArgumentParser(
+    description='Simulation for learners in echo chamber')
+parser.add_argument(
+    '--seed', metavar='r', type=int, default=42, help='seed for randomness')
+parser.add_argument(
+    '--readDirectory',
+    metavar='r',
+    type=str,
+    required=True,
+    help='directory from where to read the learner data')
+parser.add_argument(
+    '--writeDirectory',
+    metavar='w',
+    type=str,
+    required=True,
+    help='directory to where to write the learner data')
+parser.add_argument(
+    '--learnerNumbers',
+    metavar='f',
+    type=str,
+    required=True,
+    help='numbers of the learners to participate in the conversation')
+parser.add_argument(
+    '--convLength',
+    metavar='c',
+    type=int,
+    default=20,
+    help='maximum length of conversation')
+parser.add_argument(
+    '--numConversations',
+    metavar='g',
+    type=int,
+    default=30,
+    help='number of conversations')
+args = parser.parse_args()
 
-numFiles = int(sys.argv[1])
+np.random.seed(args.seed)
 
-fileNames = sys.argv[1:numFiles + 1]
+learnerNumbers = args.learnerNumbers.strip().split("\t")
+readNames = [args.readDirectory + "/" + i for i in learnerNumbers]
+writeNames = [args.writeDirectory + "/" + i for i in learnerNumbers]
 
 learners = []
 
-for i in fileNames:
-    inp = readVecspaceFile(i)
-    learners.append(VecspaceLearner(inp[0], inp[1]))
+#for i in readNames:
+#    inp = readVecspaceFile(i)
+#    learners.append(VecspaceLearner(inp[0], inp[1]))
 
-learnerProcess(learners, Ntalks=30, convlength=20)
+#learnerProcess(
+#    learners, Ntalks=args.numConversations, convlength=args.convLength)
 
-for i in range(numFiles):
-    writeVecspaceFile(sys.argv[i + 1 + numFiles], learners[i].vecspace,
-                      learners[i].C)
+#for i in range(len(writeNames)):
+#    writeVecspaceFile(writeNames[i], learners[i].vecspace, learners[i].C)
