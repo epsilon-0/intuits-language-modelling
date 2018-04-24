413f3b532a540dd01844fcb9d9eb77431d8cea0f
epsilon-0 <mathgeniusabhinav@gmail.com>
2018-04-24 17:50:38 -0400

    modified distance matrix creator to neighbours standard

--- src/make_distance_matrix.py
+++ src/make_distance_matrix.py
@@ -15,6 +15,12 @@ parser.add_argument(
     type=str,
     required=True,
     help='outputfile to output the distance matrix to')
+parser.add_argument(
+    '--num-neighbours',
+    metavar='f',
+    type=int,
+    default=50,
+    help='number of neighbours to use in making the simplicial complex')
 args = parser.parse_args()
 
 cloud = []
@@ -25,7 +31,14 @@ for i in lines:
     j = list(map(float, i.strip().split()[1:]))
     cloud.append(j)
 
-distances = distance_matrix(cloud, cloud)
+ndistances = distance_matrix(cloud, cloud)
+distances = [[100000.00 for i in range(len(cloud))] for j in range(len(cloud))]
+for i in range(len(cloud)):
+    dList = [(ndistances[i][j], j) for j in range(len(cloud))]
+    dList.sort()
+    for j in range(min(len(cloud), args.num_neighbours+1)):
+        distances[i][dList[j][1]] = dList[j][0]
+        distances[dList[j][1]][i] = dList[j][0]
 
 with open(args.outfile, "w") as out:
     out.write("\n".join([" ".join(list(map(str, i))) for i in distances]))
