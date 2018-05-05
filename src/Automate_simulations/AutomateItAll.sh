89bb99777f7ebab0d96ed5340fec07481442ede4
Roy Rinberg <royrinberg@gmail.com>
2018-05-05 18:55:08 -0400

    wrote it to be more parallelized (not just automated)

--- src/Automate_simulations/AutomateItAll.sh
+++ src/Automate_simulations/AutomateItAll.sh
@@ -2,10 +2,12 @@
 
 #SBATCH --job-name=Intuits-Simulation
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=4
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=1
 #SBATCH --mem=10GB
 #SBATCH --time=72:00:00
-
+#SBATCH --mail-user=rr2635@nyu.edu
+#SBATCH --array=0-20
 module purge
 module load python3/intel/3.6.3
 #module load parallel/20171022
@@ -52,7 +54,7 @@ do
 						echo $run
 
 workingDirectory="simulations/trial"$run"_n_"$n"_p_"$p"_mn_"$mn"_mx_"$mx"_T_"$T"_$(date +%Y%m%d_%H%M%S)"
-bash $SCRIPT_PATH "simulations" "$n" "$p" "$mn" "$mx" "$T" "$run" $workingDirectory # the trial number is used for the random_seed
+bash $SCRIPT_PATH "simulations" "$n" "$p" "$mn" "$mx" "$T" $SLURM_ARRAY_TASK_ID $workingDirectory # the trial number is used for the random_seed
    					done
 				done
 			done
